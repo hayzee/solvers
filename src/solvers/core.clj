96a9490b5749b5aa7c524a34bf212f4f03e7fee2
hayzee <hayzee@users.noreply.github.com>
2021-08-08 23:23:21 +0100

    Initial Revision.

--- src/solvers/core.clj
+++ src/solvers/core.clj
@@ -9,7 +9,7 @@
   tg?    determines if state is in its target (solved) state
   term?  determines if state is in its terminal (unsolvable) state
   f-mv   creates new moves (not states) based on current state
-  f-apmv applies moves to a state. Generates a new list of states [s] -> [s1, s2 ... sn]
+  f-apmv - applies moves to a state. Generates a new list of states [s] -> [s1, s2 ... sn]
   "
   [st tg? term? f-mv f-apmv]
   (cond
@@ -21,12 +21,15 @@
 
 
 (defn h-solver
-  ;; Depth first backtracking solver - retains history of moves
-  ;; st     - state
-  ;; tg?    - determines if state is in its target (solved) state
-  ;; term?  - determines if state is in its terminal (unsolvable) state
-  ;; f-mv   - creates new moves (not states) based on current state
-  ;; f-apmv - applys moves to a state to generate a new list of states [s] -> [s1, s2 ... sn]
+  "
+  Depth first backtracking solver - retains history of moves
+
+  st     - state
+  tg?    - determines if state is in its target (solved) state
+  term?  - determines if state is in its terminal (unsolvable) state
+  f-mv   - creates new moves (not states) based on current state
+  f-apmv - applies moves to a state. Generates a new list of states [s] -> [s1, s2 ... sn]
+  "
   [st tg? term? f-mv f-apmv]
   (cond
     (tg? st)    (list (list (hash-map :state st)))            ;; target case
@@ -35,4 +38,9 @@
                 ns (h-solver (f-apmv st mv) tg? term? f-mv f-apmv)]
             (cons mv ns))))
 
+
+(defn product
+  "Cross product of lists"
+  ([s1 s2] (for [x s1 y s2] [x y]))
+  ([s1 s2 & r] (map #(apply conj %) (apply product (cons (product s1 s2) r)))))
 
