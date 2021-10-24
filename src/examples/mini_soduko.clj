0308cec1d1f2ea495cfc001ce966ca8e4dcb2278
hayzee <hayzee@users.noreply.github.com>
2021-10-24 17:32:36 +0100

    Needs a rethink:
    1. f-term? terminal state is a full board
    2. f-valid? Check for invalid board configurations - i.e. any dups

--- src/examples/mini_soduko.clj
+++ src/examples/mini_soduko.clj
@@ -1,4 +1,6 @@
-(ns examples.mini-soduko)
+(ns examples.mini-soduko
+  (:require [solvers.core :as solvers])
+  )
 
 ;; todo: Still a W.I.P. !!!!!
 
@@ -22,49 +24,94 @@
 ;;
 ;;
 
+
+;; Helper functions
+
 (defn size
-  ; The size of a board is the count of its rows.
+  "The size of a board is the count of its rows."
   [st]
   (count st))
 
+
 (defn rows
-  ; Can just return the board for this as it is oriented by rows anyway
+  "Can just return the board for this, as it is oriented by rows anyway."
   [st]
   st)
 
+
+(defn row
+  "Return the `rth row in `st"
+  [st r]
+  (nth st r))
+
+
 (defn cols
-  ; Return each column
+  "Return each column"
   [st]
   (apply map vector st))
 
+
+(defn col
+  "Return the `cth col in `st"
+  [st c]
+  (map #(nth % c) st))
+
+
+(defn row-col-vals
+  "Returns the set of values (excluding nil) contained in the union of row 'r and col 'c in state 'st"
+  [st [r c]]
+  (set (filter identity (reduce conj (row st r) (col st c)))))
+
+
+
+
+(defn legal-mv?
+  "Return true if it is legal to apply the move `[[r c] v] to state `st. `[r c] in `st is assumed to be nil"
+  [st [[r c] v]]
+  (not (contains? v (row-col-vals st [r c])))
+  )
+
+
+
+
+
+(defn find-empties
+  "Return the row col pairs or all empty positions on the board."
+  [st]
+  (for [r (range (size st))
+        c (range (size st))
+        :when (= (get-in st [r c]) nil)]
+    [r c]))
+
+
+
 (defn term?
-  ; If all rows are filled, then there are no more moves and this is a terminal state!
+  "If all rows are filled, then there are no more moves and this is a terminal state!"
   [st]
   (->>
     (map #(count (remove nil? %)) (rows st))
     (every? #(= (size st) %))))
 
+
 (defn tg?
-  ;; If all rows and all columns add up to 9, then it's target state
+  "If all rows and all columns add up to 9, then it's target state."
   [st]
   (= {1 6, 2 6, 3 6} (frequencies (flatten (list (rows st) (cols st))))))
 
 
-(defn find-empties [st]
-  (for [r (range size)
-        c (range size)
-        :when (= (get-in st [r c]) nil)
-        ]
-    [r c]))
-
-
-(defn gen-moves [st]
+(defn f-mv
+  "Given a state, generate a bunch of possible future moves"
+  [st]
   (for [e (find-empties st)
-        n (range 1 (inc size))
-        ]
+        n (range 1 (inc (size st)))]
     [e n]))
 
-(defn apply-move [st [[r c] v]]
+
+(defn f-apmv [st [[r c] v]]
   (assoc-in st [r c] v))
 
+
+
+(defn solve [st]
+  (solvers/solver st tg? term? f-mv f-apmv))
 
--- test/examples/mini_soduko_test.clj
+++ test/examples/mini_soduko_test.clj
@@ -6,7 +6,7 @@
 
 ; setup
 
-(def solved-state
+(def solved-state    ; goal state, valid solution
   [[1 2 3]
    [3 1 2]
    [2 3 1]])
@@ -16,25 +16,64 @@
    [nil 1 nil]
    [nil 3 1]])
 
-(def failed-state    ; i.e. an invalid terminal state
+(def failed-state    ; i.e. an invalid terminal state (duplicate in col 3)
   [[1 2 1]
    [3 1 2]
    [2 3 1]])
+
+(def test-state    ; i.e. not a valid state but useful for test
+  [[1 2 3]
+   [4 5 6]
+   [7 8 9]])
 
 ; tests
 
 (deftest size-test
   (is (= 3 (size unsolved-state))))
 
+
 (deftest rows-test
   (is (= [[1 2 3]
           [3 1 2]
           [2 3 1]] (rows solved-state))))
 
+
+(deftest row-test
+  (is (= [1 2 3] (row solved-state 0)))
+  (is (= [3 1 2] (row solved-state 1)))
+  (is (= [2 3 1] (row solved-state 2))))
+
+
 (deftest cols-test
   (is (= [[1 3 2]
           [2 1 3]
           [3 2 1]] (cols solved-state))))
+
+
+(deftest col-test
+  (is (= [1 3 2] (col solved-state 0)))
+  (is (= [2 1 3] (col solved-state 1)))
+  (is (= [3 2 1] (col solved-state 2))))
+
+
+(deftest row-col-vals-test
+  (is (= #{1 2 3} (row-col-vals solved-state [0 0])))
+  (is (= #{1 2 3} (row-col-vals solved-state [0 1])))
+  (is (= #{1 2 3} (row-col-vals solved-state [0 2])))
+  (is (= #{1 2 3} (row-col-vals solved-state [1 0])))
+  (is (= #{1 2 3} (row-col-vals solved-state [1 1])))
+  (is (= #{1 2 3} (row-col-vals solved-state [1 2])))
+  (is (= #{1 2 3} (row-col-vals solved-state [2 0])))
+  (is (= #{1 2 3} (row-col-vals solved-state [2 1])))
+  (is (= #{1 2 3} (row-col-vals solved-state [2 2]))))
+
+
+(deftest find-empties-test
+  (is (not (empty? (find-empties unsolved-state))) "An unsolved state must have at least one possible move")
+  (is (empty? (find-empties solved-state)) "A solved state cannot have any valid moves")
+  (is (empty? (find-empties failed-state)) "A failed state cannot have any valid moves")
+  (is (= (find-empties unsolved-state) '([0 2] [1 0] [1 2] [2 0])) "Unsolved state should generate specific set of empties"))
+
 
 (deftest term?-test
   (is (= false (term? unsolved-state)) "An unsolved state cannot be in a terminal state")
@@ -46,7 +85,6 @@
   (is (= false (tg? unsolved-state)) "An unsolved state cannot be in a target state")
   (is (= true (tg? solved-state)) "An solved state must be in the target state")
   (is (= false (tg? failed-state)) "A failed state cannot be in a target state"))
-
 
 
 (comment
@@ -69,5 +107,3 @@
   (map #(apply-move st %) (gen-moves st))
 
   )
-
-
